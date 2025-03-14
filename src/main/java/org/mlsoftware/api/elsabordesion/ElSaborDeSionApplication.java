cbb7779180d024c6ec2c177bb298151ea0bcb1e4
Mike Lauren <110873019+EmanuelObredor18@users.noreply.github.com>
2025-03-14 15:35:39 -0500

    Init repository

---
+++ src/main/java/org/mlsoftware/api/elsabordesion/ElSaborDeSionApplication.java
@@ -0,0 +1,13 @@
+package org.mlsoftware.api.elsabordesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElSaborDeSionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElSaborDeSionApplication.class, args);
+	}
+
+}
